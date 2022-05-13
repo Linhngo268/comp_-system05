6e15530285e343f8c05a1a6ad21a6244f8a1d4a4
Linhngo268 <83965979+Linhngo268@users.noreply.github.com>
2022-05-13 23:21:07 +0930

    Add files via upload

---
+++ prac5/Abs.asm
@@ -0,0 +1,25 @@
+// Calculates the absolute value of R1 and stores the result in R0.
+// (R0, R1 refer to RAM[0], and RAM[1], respectively.)
+
+// Put your code here.
+//If R1>0
+//R1=R0;
+//else 
+//R1=-R0
+
+@R1
+D=M
+@POSITIVE
+D;JGT
+
+@R0
+M=M-D
+@END
+0;JMP
+
+(POSITIVE)
+@R0
+M=D
+(END)
+@END
+0;JMP
---
+++ prac5/Mult.asm
@@ -0,0 +1,39 @@
+// This file is based on part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: Mult.asm
+
+// Multiplies R1 and R2 and stores the result in R0.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+ @R0
+ M=0
+
+@R1 
+D=M
+ 
+ @i
+ M=D
+ 
+@END
+D;JEQ
+
+(LOOP)
+@R2
+D=M
+
+@R0
+M=D+M
+
+@i
+D=M
+D=D-1
+M=D
+
+@LOOP
+D;JGT
+
+(END)
+@END
+0;JMP
